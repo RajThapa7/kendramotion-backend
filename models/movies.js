6e469c7a9b65548027c60164d36dd129cce5699a
nsubash38 <neupanesubash38@gmail.com>
2023-05-17 16:56:42 +0545

    added movies model

---
+++ models/movies.js
@@ -0,0 +1,14 @@
+const movieSchema = new mongoose.Schema({
+    title: String,
+    name: String,
+    url: String,
+    thumbImage: String,
+    position: Number,
+    artist: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'ArtistProfile',
+    },
+  });
+
+
+  module.exports = movieSchema; 
