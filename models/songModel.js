f8de091f19b82c2c3b14a222388250c6263b8401
Prajwalad101 <prajwalad101@gmail.com>
2023-05-23 18:16:49 +0545

    refactor: remove thumbImage field from songModel

--- models/songModel.js
+++ models/songModel.js
@@ -4,7 +4,6 @@ const songSchema = new mongoose.Schema({
   title: String,
   name: String,
   url: String,
-  thumbImage: String,
   position: Number,
   artist: {
     type: mongoose.Schema.Types.ObjectId,
