09c2ce9876d6348a847e16765c1e67792ebf2bce
Prajwalad101 <prajwalad101@gmail.com>
2023-06-05 21:15:54 +0545

    refactor: allow cors for multiple client urls

--- middlewares/corsMiddleware.js
+++ middlewares/corsMiddleware.js
@@ -1,16 +1,26 @@
 const cors = require("cors");
 const AppError = require("../utils/appError");
 
+const origin1 = process.env.CLIENT_URL1;
+const origin2 = process.env.CLIENT_URL2;
+
+const whitelist = [origin1, origin2];
+
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+
 function corsMiddleware() {
-  const origin = process.env.CLIENT_URL;
-  if (!origin) {
+  if (!origin1 || !origin2) {
     throw new AppError("CLIENT_URL not defined", 500);
   }
-
-  const corsOptions = {
-    origin,
-    optionsSuccessStatus: 200,
-  };
 
   return cors(corsOptions);
 }
